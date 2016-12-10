4b5504fa7824c89420cd9356d4c7fb38575321f9
Eugen Mirovschi <emirovschi@vmists.com>
2016-12-10 12:52:58 +0200

    Removing unecessary rules

--- src/main/java/com/emirovschi/midps3/oauth/OAuth2SecurityConfiguration.java
+++ src/main/java/com/emirovschi/midps3/oauth/OAuth2SecurityConfiguration.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -36,12 +35,6 @@ public class OAuth2SecurityConfiguration extends WebSecurityConfigurerAdapter
     public void configure(WebSecurity web) throws Exception
     {
         web.ignoring().antMatchers("/h2-console/**");
-    }
-
-    @Override
-    protected void configure(final HttpSecurity http) throws Exception
-    {
-        http.authorizeRequests().antMatchers("/oauth/token").permitAll();
     }
 
     @Override
