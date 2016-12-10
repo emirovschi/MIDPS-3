fcf3384fd480053f9bc2c5214e19ade85cdf11db
Eugen Mirovschi <emirovschi@vmists.com>
2016-12-10 20:57:56 +0200

    Adding post repository

---
+++ src/main/java/com/emirovschi/midps3/posts/PostRepository.java
@@ -0,0 +1,19 @@
+package com.emirovschi.midps3.posts;
+
+import com.emirovschi.midps3.posts.models.PostModel;
+import com.emirovschi.midps3.posts.models.TagModel;
+import com.emirovschi.midps3.users.models.UserModel;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Set;
+
+@Repository
+public interface PostRepository extends JpaRepository<PostModel, Long>
+{
+    Page<PostModel> findByTitleLikeAndTagsInAndUserIn(String title, Set<TagModel> tags, Set<UserModel> users, Pageable pageable);
+
+    PostModel findById(long id);
+}
