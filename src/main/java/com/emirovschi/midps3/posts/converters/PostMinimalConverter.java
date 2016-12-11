aff218c7915e799c79ead1d03a2b984898c691e3
Eugen Mirovschi <emirovschi@vmists.com>
2016-12-11 13:00:51 +0200

    Adding post converter

---
+++ src/main/java/com/emirovschi/midps3/posts/converters/PostMinimalConverter.java
@@ -0,0 +1,28 @@
+package com.emirovschi.midps3.posts.converters;
+
+import com.emirovschi.midps3.converters.Converter;
+import com.emirovschi.midps3.posts.dto.PostDTO;
+import com.emirovschi.midps3.posts.models.PostModel;
+import org.springframework.stereotype.Component;
+
+import static java.util.Collections.emptyMap;
+import static java.util.Optional.ofNullable;
+
+@Component("postMinimalReverseConverter")
+public class PostMinimalConverter implements Converter<PostModel, PostDTO>
+{
+    @Override
+    public PostDTO convert(final PostModel post)
+    {
+        final PostDTO postDTO = new PostDTO();
+        postDTO.setTitle(post.getTitle());
+        postDTO.setId(post.getId());
+        postDTO.setScore(getScore(post));
+        return postDTO;
+    }
+
+    private int getScore(final PostModel post)
+    {
+        return ofNullable(post.getVotes()).orElse(emptyMap()).values().stream().mapToInt(i -> i).sum();
+    }
+}
