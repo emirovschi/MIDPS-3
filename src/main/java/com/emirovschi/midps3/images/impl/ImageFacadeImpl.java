e64f80f54bac654cf0b9eb8339ee7a1bdf5a985e
Eugen Mirovschi <emirovschi@vmists.com>
2016-12-14 21:16:12 +0200

    Fix image resizing

--- src/main/java/com/emirovschi/midps3/images/impl/ImageFacadeImpl.java
+++ src/main/java/com/emirovschi/midps3/images/impl/ImageFacadeImpl.java
@@ -5,14 +5,15 @@ import com.emirovschi.midps3.images.ImageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
 import static java.awt.image.BufferedImage.TYPE_INT_ARGB;
 import static java.awt.image.BufferedImage.TYPE_INT_RGB;
-import static javax.imageio.ImageIO.*;
+import static javax.imageio.ImageIO.read;
+import static javax.imageio.ImageIO.write;
 import static org.springframework.http.MediaType.IMAGE_JPEG_VALUE;
 
 @Component
@@ -24,9 +25,21 @@ public class ImageFacadeImpl implements ImageFacade
     @Override
     public byte[] getPreview(final byte[] image, final String imageType) throws IOException
     {
-        final int type = imageType.equals(IMAGE_JPEG_VALUE) ? TYPE_INT_RGB : TYPE_INT_ARGB;
-        ByteArrayOutputStream stream = new ByteArrayOutputStream();
-        write(imageService.getPreview(read(new ByteArrayInputStream(image)), type), imageType, stream);
+        final BufferedImage original = read(new ByteArrayInputStream(image));
+        final BufferedImage resized = imageService.getPreview(original, getType(imageType));
+
+        final ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        write(resized, getFormat(imageType), stream);
         return stream.toByteArray();
+    }
+
+    private int getType(final String imageType)
+    {
+        return imageType.equals(IMAGE_JPEG_VALUE) ? TYPE_INT_RGB : TYPE_INT_ARGB;
+    }
+
+    private String getFormat(final String imageType)
+    {
+        return imageType.replace("image/", "");
     }
 }
