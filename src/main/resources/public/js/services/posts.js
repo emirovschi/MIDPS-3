f2205fbc44797d11ebdff7d8cd694ea5ab9b67bb
Eugen Mirovschi <emirovschi@vmists.com>
2016-12-17 17:23:33 +0200

    Sending vote requests

--- src/main/resources/public/js/services/posts.js
+++ src/main/resources/public/js/services/posts.js
@@ -1,12 +1,22 @@
 app.service('posts', function($http)
 {
-    this.getPosts = function (page, size, searchData)
+    this.getPosts = function(page, size, searchData)
     {
         return $http.post("/posts/search?page=" + page + "&size=" + size, searchData);
     }
 
-    this.getVotes = function (size, searchData)
+    this.getVotes = function(size, searchData)
     {
         return $http.post("/posts/votes?page=0&size=" + size, searchData);
+    }
+
+    this.voteUp = function(id)
+    {
+        return $http.post("/posts/" + id + "/vote/up");
+    }
+
+    this.voteDown = function(id)
+    {
+        return $http.post("/posts/" + id + "/vote/down");
     }
 });
